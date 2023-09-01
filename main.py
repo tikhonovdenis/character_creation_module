a336648c759ac32aa27b680aa0ef17dc37126f7f
Denis Tikhonov <denistikhonov@yandex.ru>
2023-09-01 19:21:00 +0300

    add main

--- main.py
+++ main.py
@@ -109,5 +109,4 @@ if __name__ == '__main__':
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
+    print(start_training(choice_char_class(char_name)))
